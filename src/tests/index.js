0fd4d2c9610a359fd65a952d48b41228db00ce7f
Jajabenit250 <jajabenit@gmail.com>
2020-08-08 09:26:52 +0200

    configurations of - helpers - resolvers middlewares - confgired apollo graphql

--- src/tests/index.js
+++ src/tests/index.js
@@ -1,16 +1,62 @@
-import chai from 'chai';
-import chaiHttp from 'chai-http';
-import app from '../app';
+// import { expect } from 'chai';
+// import * as userApi from './api';
 
-chai.use(chaiHttp);
-chai.should();
+// describe('Create user', () => {
+//   it('user creating account', async () => {
+//     // sign up
 
-describe('App Tests', () => {
-  it('should not return the homepage', (done) => {
-    chai.request(app).get('/')
-      .end((err, res) => {
-        res.should.have.status(404);
-        done();
-      });
-  });
+//     let {
+//       data: {
+//         data: {
+//           createUser: { token },
+//         },
+//       },
+//     } = await userApi.createUser({
+//       email:"benny.ogidan@benny.com",
+//       firstName:"RUKUNDO", 
+//       lastName:"thomas",
+//       isVerified:true
+      
+//     });
+//     // const token = 'eooweornfnwiiiei123wefinsdnf'
+//     const {
+//       data: {
+//         data: { me },
+//       },
+//     } = await userApi.me(token);
+//     console.log('==-=-=-=-=->>', me);
+//     expect(me).to.eql({
+//       id: '3',
+//       email:"benny.ogidan@benny.com",
+//       firstName:"RUKUNDO", 
+//       lastName:"thomas",
+//       isVerified:true
+//     });
+
+
+
+
+//   });
+// });
+
+const chai = require('chai');
+
+const expect = chai.expect;
+const url = `http://localhost:8000`;
+const request = require('supertest')(url);
+
+describe('GraphQL', () => {
+    it('Create user', (done) => {
+        
+        request.post('/graphql')
+        .send( 'mutation{ createUser(email:"benny.ogidan@benny.com",firstName:"RUKUNDO",lastName:"thomas",isVerified:true) { token } }')
+        .expect(200)
+        .end((err,res) => {
+            // res will contain array with one user
+            if (err) return done(err);
+            // res.body.should.have.property('token')
+            done();
+        })
+    })
+
 });
