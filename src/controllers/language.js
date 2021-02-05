43a127bddc353fea9fd921a99ae7ac6a9871e5bc
Niyonsengaeric <niyeric11@gmail.com>
2021-02-05 06:35:47 +0200

    ft: get specific language
    - allow get specific languge
    - all save user test results
    - all get user learning languages
    - allow get language by country

--- src/controllers/language.js
+++ src/controllers/language.js
@@ -3,6 +3,8 @@ import CountryServices from '../services/country';
 import CoursesServices from '../services/courses';
 import response from '../helpers/response';
 import LanguageHelper from '../helpers/languages.helper';
+import UserServices from '../services/users';
+
 
 class LanguageController {
 	static async addLanguage(req, res) {
@@ -66,7 +68,7 @@ class LanguageController {
 				await Promise.all(
 					languages.rows.map(async (course1, index) => {
 						if (primaryLanguageId !== languages.rows[index].dataValues.id) {
-							const { name } = languages.rows[index].dataValues;							
+							const { name } = languages.rows[index].dataValues;
 							if (name !== 'English') {
 								availableLanguage.push(languages.rows[index].dataValues);
 							}
@@ -196,6 +198,90 @@ class LanguageController {
 				'Enrolled languages',
 				200,
 				languagesResponse,
+			);
+		} catch (e) {
+			return response.errorMessage(res, e.message, 500);
+		}
+	}
+
+	static async singleLanguage(req, res) {
+		try {
+			const { id } = req.params;
+			if (isNaN(id)) {
+				return response.errorMessage(res, 'id must be an integer', 400);
+			}
+			const language = await LanguageServices.getLanguage(id);
+			if (language) {
+				const findCountry = await CountryServices.findCountryById(
+					language.countryId,
+				);
+				const data = {
+					id: language.id,
+					LanguageName: language.name,
+					country: findCountry.name,
+					countryFlag: findCountry.flag,
+				};
+				return response.successMessage(res, 'Language details', 200, data);
+			}
+			return response.errorMessage(res, 'language not registered', 404);
+		} catch (e) {
+			return response.errorMessage(res, e.message, 500);
+		}
+	}
+
+	static async getLanguagesByCountry(req, res) {
+		try {
+			const { countryName } = req.params;
+			const findCountry = await CountryServices.findCountry(countryName);
+			if (!findCountry) {
+				return response.errorMessage(res, 'Country not registered', 404);
+			}
+			const languages = await LanguageServices.findLanguageByCountryId(
+				findCountry.id,
+			);
+			if (languages) {
+				return response.successMessage(
+					res,
+					`${countryName} Languages`,
+					200,
+					languages,
+				);
+			}
+			return response.errorMessage(
+				res,
+				'No language found for the entered country',
+				404,
+			);
+		} catch (e) {
+			return response.errorMessage(res, e.message, 500);
+		}
+	}
+
+	static async getUsersLearningLanguages(req, res) {
+		try {
+			const getUsers = await UserServices.getUsers();
+			const userLearningLanguages = [];
+			let a = -1;
+			await Promise.all(
+				getUsers.rows.map(async () => {
+					const { id } = getUsers.rows[(a += 1)].dataValues;
+					const enrolledLanguage = await LanguageServices.getEnrolledLanguages(
+						id,
+					);
+					if (enrolledLanguage.length > 0) {
+						userLearningLanguages.push({
+							user: getUsers.rows[a].dataValues,
+							enrolledLanguage,
+						});
+					}
+				}),
+			);
+
+			return response.successMessage(
+				res,
+				'user Enrolled languages',
+				200,
+				userLearningLanguages,
 			);
 		} catch (e) {
 			return response.errorMessage(res, e.message, 500);
