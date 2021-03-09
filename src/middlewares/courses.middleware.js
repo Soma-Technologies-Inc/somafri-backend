f1765d4809194fd34c83ce65f4cb93f322d4aeec
Niyonsengaeric <niyeric11@gmail.com>
2021-03-09 07:22:50 +0200

    ft: allow update and delete a course
    - allow addmin to update existing root course
    - allow admin to delete existing root course
    - allow admin to edit a course
    - allow admin to delete a course

--- src/middlewares/courses.middleware.js
+++ src/middlewares/courses.middleware.js
@@ -7,18 +7,24 @@ import CoursesServices from '../services/courses';
 class CoursesMiddleware {
 	static async findIfLanguageExist(req, res, next) {
 		const { languageId } = req.body;
-		const language = await CoursesServices.findIfLanguageExist(languageId);
-		if (language === null) {
-			return response.errorMessage(res, 'The language doesnt exist', 404);
+
+		if (languageId) {
+			const language = await CoursesServices.findIfLanguageExist(languageId);
+			if (language === null) {
+				return response.errorMessage(res, "The language doesn't exist", 404);
+			}
 		}
 		return next();
 	}
 
 	static async findIfLevelExist(req, res, next) {
 		const { levelId } = req.body;
-		const level = await CoursesServices.findIfLevelExist(levelId);
-		if (level === null) {
-			return response.errorMessage(res, 'The level doesnt exist', 404);
+
+		if (levelId) {
+			const level = await CoursesServices.findIfLevelExist(levelId);
+			if (level === null) {
+				return response.errorMessage(res, 'The level doesnt exist', 404);
+			}
 		}
 		return next();
 	}
@@ -31,6 +37,53 @@ class CoursesMiddleware {
 		if (rootCourse === null) {
 			return response.errorMessage(res, 'The root course does not exist', 404);
 		}
+		return next();
+	}
+
+	static async findIfRootCourse(req, res, next) {
+		const isNumber = value => !Number.isNaN(Number(value));
+
+		const { id } = req.params;
+		if (isNumber(id) === false) {
+			return response.errorMessage(res, 'your root course id must be an integer', 400);
+		}
+		const rootCourse = await CoursesServices.findIfRootCourseExist(
+			id,
+		);
+		if (rootCourse === null) {
+			return response.errorMessage(res, 'The root course does not exist', 404);
+		}
+		return next();
+	}
+
+	static async findIfCourse(req, res, next) {
+		const isNumber = value => !Number.isNaN(Number(value));
+		const { id } = req.params;
+		if (isNumber(id) === false) {
+			return response.errorMessage(res, 'your course id must be an integer', 400);
+		}
+		const course = await CoursesServices.findCourseByField(
+			'id', id
+		);
+		if (course.length <= 0) {
+			return response.errorMessage(res, 'The course does not exist', 404);
+		}
+
+		if (req.body.languageId) {
+			const language = await CoursesServices.findIfLanguageExist(req.body.languageId);
+			if (language === null) {
+				return response.errorMessage(res, "The language doesn't exist", 404);
+			}
+		}
+		if (req.body.rootCourseId) {
+			const rootCourse = await CoursesServices.findIfRootCourseExist(
+				req.body.rootCourseId,
+			);
+			if (rootCourse === null) {
+				return response.errorMessage(res, 'The course does not exist', 404);
+			}
+		}
+
 		return next();
 	}
 }
